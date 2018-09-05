05f652c79c5c06f983e013158b9a4eb1f7f7aec5
Rob Parker <into.the.parker@gmail.com>
2018-09-05 17:04:07 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,8 +19,8 @@ def monopoly_with_second_tier
 		monopoly = {
 	  :railroads => {
 	           :pieces => 4,
-	           :name => {},
-	           :rent_in_dollars => {},
+	           :names => {},
+	           :rent_in_dollars => {}
 	           
 	  }         
 	}
